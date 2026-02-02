954d81afc26a0ce92fe8bb1483d59c04b11e9692
naveenkumar0007 <naveenkumar45491@gmail.com>
2026-02-02 19:13:31 +0530

    quiz page added

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  document.getElementById("btns").addEventListener("click", function () {
+    window.location.href = "quiz.html";
+  });
+});
+
